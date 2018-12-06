198388e7c46325e08b620009ecfc2898039ff86f
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-06 18:52:20 -0500

    Mumuki Export on 2018-12-06 18:52:20 -0500

--- 00009_¿Qué tal si_/test.py
+++ 00009_¿Qué tal si_/test.py
@@ -1,1 +1,11 @@
-#deffffde
+describe("", function() {
+  it("Declarada la variable diaDeSemana", function() {
+    assert.notEqual(diaDeSemana, "undefined");
+  });
+    it("Le asigna el valor \"domingo\" a  diaDeSemana", function() {
+    assert.equal(diaDeSemana, "domingo");
+  });
+    it("debería imprimir ''Hoy se juega al futbol!!!''", function() {
+    assert.equal(console.toString(), "Hoy se juega al futbol!!!");
+  });
+})
