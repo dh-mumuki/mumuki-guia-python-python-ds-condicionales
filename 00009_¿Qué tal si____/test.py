d154b2ff10c17d328335a870519c33d881e3911b
andres@digitalhouse.com <andres@digitalhouse.com>
2019-06-18 23:49:16 +0000

    Mumuki Export on 2019-06-18 23:49:16 +0000

--- 00009_¿Qué tal si____/test.py
+++ 00009_¿Qué tal si____/test.py
@@ -1,5 +1,5 @@
 class TestFixtures(unittest.TestCase):
   
   def test_dia_de_semana(self):
-    self.assertTrue(dia_de_semana in globals(), 'No se ha definido la variable dia_de_semana')
     self.assertTrue(dia_de_semana == 'domingo', 'El valor de dia de semana es incorrecto.')
+    
