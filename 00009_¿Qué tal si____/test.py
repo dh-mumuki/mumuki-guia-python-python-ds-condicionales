e3b6e5d9770dbfcf61ac623684d6b2b55453b889
mrios@digitalhouse.com <mrios@digitalhouse.com>
2019-06-24 13:48:50 +0000

    Mumuki Export on 2019-06-24 13:48:50 +0000

--- 00009_¿Qué tal si____/test.py
+++ 00009_¿Qué tal si____/test.py
@@ -1,4 +1,4 @@
 class TestFixtures(unittest.TestCase):
   
   def test_dia_de_semana(self):
-    self.assertTrue(dia_de_semana.lower() == 'domingo'.lower(), 'El valor de dia de semana es incorrecto.')
+    self.assertTrue(dia_de_semana == 'domingo'.lower(), 'El valor de dia de semana es incorrecto.')
